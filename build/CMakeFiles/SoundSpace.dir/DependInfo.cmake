be563f47674bf46455e93cdd81c7c22f8b6f6181
Данил Ионин <danil.ionin.84@mail.ru>
2025-09-27 14:30:13 +0700

    Настройка CmakeLists.txt

---
+++ build/CMakeFiles/SoundSpace.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "SoundSpace_autogen/timestamp" "custom" "SoundSpace_autogen/deps"
+  "/home/ionin/Рабочий стол/kursach/build/SoundSpace_autogen/mocs_compilation.cpp" "CMakeFiles/SoundSpace.dir/SoundSpace_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/SoundSpace.dir/SoundSpace_autogen/mocs_compilation.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SoundSpace_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SoundSpace_autogen"
+  "SoundSpace_autogen/mocs_compilation.cpp"
+  "SoundSpace_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/SoundSpace_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
